bcc4c2aaa5e0b5bbc0bacfad80a2ed3c1ac22ee5
angievig <angievig@hedwig.local>
2022-03-31 19:05:13 -0500

    update a la clase 1 de la semana 10

--- Semana10/figurasModificado/src/model/FiguresManager.java
+++ Semana10/figurasModificado/src/model/FiguresManager.java
@@ -3,10 +3,16 @@ package model;
  * Case study for the Algorithms and programming course
  * @author avillota
  * @since october 2020
- * @version 1.0
+ * @version 2.0 updated on March 2022
  */
 public class FiguresManager {
+	
+	public static final int MAX_CIRCLES = 10;
+
 	private String title;
+
+	private Triangle aTriangle;
+	private Circle [] circles;
 	
 
 	
@@ -15,10 +21,21 @@ public class FiguresManager {
 	 * @param tit is a string with the figures manager title tit!= null, tit!= ""
 	 */
 
-	
+	public FiguresManager(String tit){
+
+		title = tit;
+
+		//init
+		circles= new Circle[MAX_CIRCLES]; 
+
+	}
 
 
-	
+
+
+	/**This method creates three objects of teh class Point, creates 
+	 * an object of the class Triangle and sets up the 
+	 * */
 	public void addTriangle(double xA, double yA, double xB, double yB, double xC, double yC) {
 		
 	}
@@ -44,6 +61,20 @@ public class FiguresManager {
 		return out;
 	}
 	
+
+	/**
+	 * @return true si hay triangulo, false en el caso contrario
+	 * */
+	 public boolean hasTriangle(){
+	 	bool out=false;
+
+	 	if(aTriangle!= null){
+	 		out = true;
+	 	}
+
+	 	return out;
+	 }
+
 	
 	//getters and setters
 	public String getTitle() {
@@ -52,13 +83,5 @@ public class FiguresManager {
 	public void setTitle(String tit) {
 		title = tit;
 	}
-	public Triangle getaTriangle() {
-		return aTriangle;
-	}
-	public Circle getaCircle() {
-		return aCircle;
-	}
-	public Rectangle getaRectangle() {
-		return aRectangle;
-	}
+	+
 }
---
+++ Semana10/figurasModificado/src/ui/Main.java
@@ -0,0 +1,197 @@
+package ui;
+
+import java.util.Scanner;
+
+import model.FiguresManager;
+
+public class Main {
+	
+	
+
+	//atributos
+	private FiguresManager figures;
+	private Scanner sc;
+	
+	//Constructor
+	public Main() {
+		figures= new FiguresManager("Example");
+		sc= new Scanner(System.in);
+	}
+	public static void main(String [] args) {
+		
+		System.out.println("Iniciando la aplicación");
+		
+		Main ppal= new Main();
+		
+		int option=0;
+		
+		do{
+			option= ppal.showMenu();
+			ppal.executeOperation(option);
+			
+		}while (option!=0);
+		
+	}
+	
+	public int showMenu() {
+		int option=0;
+
+		System.out.println(
+				"Seleccione una opción para empezar\n" +
+				"(1) Para crear el triangulo\n" +
+				"(2) Para crear el rectángulo\n"+
+				"(3) Para crear el círculo  \n"+
+				"(4) Para mostrar las figuras\n" +  
+				"(0) Para salir"
+				);
+		option= sc.nextInt();
+		sc.nextLine();
+		return option;
+	}
+	
+	public void executeOperation(int operation) {
+		
+		switch(operation) {
+		case 0:
+			System.out.println("Bye!");
+			break;
+		case 1:
+			createTriangle() ;
+			break;
+		case 2:
+			createRectangle();
+			break;
+	
+		case 3:
+			createCircle();
+			break;
+
+		case 4:
+			showFigures();
+			break;
+		
+		default:
+			System.out.println("Error, opción no válida");
+		
+		}
+	}
+
+	public void createRectangle() {
+		
+
+
+		System.out.println("Creando un rectángulo");
+		double x1, y1;
+		System.out.println("Creando el punto 1");
+		System.out.println("Por favor digite x");
+		x1= sc.nextDouble();
+		
+		System.out.println("Por favor digite y");
+		y1= sc.nextDouble();
+		
+		double x2, y2;
+		System.out.println("Creando el punto 2");
+		System.out.println("Por favor digite x");
+		x2= sc.nextDouble();
+		
+		System.out.println("Por favor digite y");
+		y2= sc.nextDouble();
+		
+		double x3, y3;
+		System.out.println("Creando el punto 3");
+		System.out.println("Por favor digite x");
+		x3= sc.nextDouble();
+		
+		System.out.println("Por favor digite y");
+		y3= sc.nextDouble();
+		
+		double x4, y4;
+		System.out.println("Creando el punto 4");
+		System.out.println("Por favor digite x");
+		x4= sc.nextDouble();
+		
+		System.out.println("Por favor digite y");
+		y4= sc.nextDouble();
+		
+		figures.addRectangle(x1, y1, x2, y2, x3, y3, x4, y4);
+	}
+	public void createTriangle() {
+		System.out.println("Creando un triángulo");
+		
+		if (figures.hasTriangle()){
+			System.out.println("El triangulo existe, desea sobreescribir?");
+			System.out.println(
+				"Seleccione una opción \n" +
+				"(1) Para sobreescribir\n" +
+				"Cualquier otro numero para terminar\n"+);
+
+			int input = sc.nextInt();
+
+			if(input ==1){
+				createNewTriangle();
+				System.out.println("El triangulo fue sobreescrito");
+			}else{
+				System.out.println("No hubo cambios y termina la acción");
+
+			}
+
+		} else{
+			createNewTriangle();
+			System.out.println("El triangulo fue creado");
+		}
+
+		
+		
+	}
+
+	public void createNewTriangle(){
+
+		double x1, y1;
+		System.out.println("Creando el punto 1");
+		System.out.println("Por favor digite x");
+		x1= sc.nextDouble();
+		
+		System.out.println("Por favor digite y");
+		y1= sc.nextDouble();
+		
+		double x2, y2;
+		System.out.println("Creando el punto 2");
+		System.out.println("Por favor digite x");
+		x2= sc.nextDouble();
+		
+		System.out.println("Por favor digite y");
+		y2= sc.nextDouble();
+		
+		double x3, y3;
+		System.out.println("Creando el punto 3");
+		System.out.println("Por favor digite x");
+		x3= sc.nextDouble();
+		
+		System.out.println("Por favor digite y");
+		y3= sc.nextDouble();
+		
+		figures.addTriangle(x1, y1, x2, y2, x3, y3);
+
+	}
+
+	public void createCircle() {
+		double x, y, r;
+		System.out.println("Por favor digite el radio");
+		r= sc.nextDouble();
+		
+		System.out.println("Por favor digite x");
+		x= sc.nextDouble();
+		
+		System.out.println("Por favor digite y");
+		y= sc.nextDouble();
+		
+		figures.addCircle(x, y, r);
+	}
+	
+	public void showFigures() {
+		
+		System.out.println(figures.getFigures());
+		
+	}
+
+}
